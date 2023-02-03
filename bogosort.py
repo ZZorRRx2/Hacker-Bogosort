24fb362df24cd1dfc70865c58617adfaa25e2e9b
Matthew To <matthewto2is@gmail.com>
2023-02-03 09:11:14 +0000

    The python file
    The python file is uploaded

---
+++ bogosort.py
@@ -0,0 +1,34 @@
+# Python program for implementation of Bogo Sort
+import random
+import time
+
+# Sorts array a[0..n-1] using Bogo sort
+def bogoSort(a):
+    n = len(a)
+    while (is_sorted(a)== False):
+        shuffle(a)
+
+# To check if array is sorted or not
+def is_sorted(a):
+    print(a)
+    time.sleep(0.25)
+    n = len(a)
+    for i in range(0, n-1):
+        if (a[i] > a[i+1] ):
+            return False
+    return True
+
+# To generate permutation of the array
+def shuffle(a):
+    n = len(a)
+    for i in range (0,n):
+        r = random.randint(0,n-1)
+        a[i], a[r] = a[r], a[i]
+
+# Driver code to test above
+#a = [3, 2, 4, 1, 0, 5]
+a = [random.randint(0, 1) for iter in range(2520)]
+bogoSort(a)
+print("Sorted array :")
+for i in range(len(a)):
+    print ("%d" %a[i]),
